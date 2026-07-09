a3b8fe2f3c72348094cf0589b62446fa35796fce
Nicole <github.confess492@passmail.net>
2026-07-09 10:29:05 -0400

    Update endpoint url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quoteContainer = document.querySelector("#quote")
 const authorContainer = document.querySelector("#author")
 
-const quote = fetch("https://stoic-quotes.com/api/quote")
+const quote = fetch("https://www.stoic-quotes.com/api/quote")
   .then((response) => response.json())
   .then((data) => {
     quoteContainer.innerHTML = data.text
